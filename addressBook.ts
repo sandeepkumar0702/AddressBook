64ef600a3cccb176c700d8f885e9859b58ffd2d5
Sandeep Kumar <sandeepkumar073113@gmail.com>
2025-01-23 10:46:13 +0530

    [ Sandeep ] Modify: Modify UseCase1

--- addressBook.ts
+++ addressBook.ts
@@ -1,39 +1,61 @@
-const readlineSync = require('readline-sync');
-class Contact {
-    firstName: string;
-    lastName: string;
-    address: string;
-    city: string;
-    state: string;
-    zip: string;
-    phoneNumber: string;
-    email: string;
-  
-    constructor(
-        firstName: string,
-        lastName: string,
-        address: string,
-        city: string,
-        state: string,
-        zip: string,
-        phoneNumber: string,
-        email: string
-    ){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+import * as readline from "readline-sync";
+
+interface Contact {
+  firstName: string;
+  lastName: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  phoneNumber: string;
+  email: string;
+}
+
+class AddressBook {
+  private contacts: Contact[];
+
+  constructor() {
+    this.contacts = [];
+  }
+
+  addContact(): void {
+    console.log("\nEnter Contact Details:");
+    const firstName = readline.question("First Name: ");
+    const lastName = readline.question("Last Name: ");
+    const address = readline.question("Address: ");
+    const city = readline.question("City: ");
+    const state = readline.question("State: ");
+    const zip = readline.question("ZIP Code: ");
+    const phoneNumber = readline.question("Phone Number (10 digits): ");
+    const email = readline.question("Email: ");
+
+    const contact: Contact = {
+      firstName,
+      lastName,
+      address,
+      city,
+      state,
+      zip,
+      phoneNumber,
+      email,
+    };
+
+    this.contacts.push(contact);
+    console.log("Contact added successfully!");
+  }
+
+  displayContacts(): void {
+    if (this.contacts.length === 0) {
+      console.log("No contacts available.");
+    } else {
+      console.log("\nContacts List:");
+      this.contacts.forEach((contact, index) => {
+        console.log(`
+          ${index + 1}. ${contact.firstName} ${contact.lastName}, Address: ${contact.address}, ${contact.city}, ${contact.state}, ${contact.zip}, Phone: ${contact.phoneNumber}, Email: ${contact.email}
+        `);
+      });
     }
   }
-  // AddressBook
-class AddressBook {
-    private contacts: Contact[] = [];
 }
 
 const addressBook = new AddressBook();
-
-console.log(addressBook);
